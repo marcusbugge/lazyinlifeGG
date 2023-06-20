1c5d8d632cdaa5483e7ba6cde77aa66bd4d72602
marcusbugge <marcus_bugge@hotmail.com>
2023-06-20 22:45:41 +0200

    upload

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,79 @@
-import React from 'react'
+import React, { Component } from "react";
+import "./footer.css";
+import logo from "../../assets/logo.png";
+import discord from "../../assets/discord.png";
+import twitter from "../../assets/twitter.png";
+import twitch from "../../assets/twitch.png";
+import { Link } from "react-router-dom";
+import isOnline from "../../App";
+import axios from "axios";
 
-export default function Footer() {
-  return (
-    <div className="footer">Footer</div>
-  )
+export class Footer extends Component {
+  render() {
+    async function logout() {
+      let url = "/api/user/logout";
+      axios
+        .post(url)
+        .then((res) => {
+          console.log(res);
+          localStorage.clear();
+          window.location.reload(false);
+        })
+        .catch((err) => console.log(err));
+    }
+    return (
+      <div className="footer-cnt">
+        <div className="footer-content">
+          <div className="staff-cnt">
+            <h1>LAZY IN LIFE</h1>
+            <div className="stripe-mini"></div>
+
+            <div className="games">
+              <Link to="/games/valorant">News</Link>
+              <Link to="/aboutus">About us</Link>
+              <Link to="/games/csgo">Staff</Link>
+              {localStorage.getItem("isAuthenticated") ? (
+                <Link to="/" onClick={logout}>
+                  {" "}
+                  Logout
+                </Link>
+              ) : (
+        ""
+              )}
+            </div>
+          </div>
+          <div className="games-cnt">
+            <h1>OUR TEAMS</h1>
+            <div className="stripe-mini"></div>
+
+            <div className="games">
+              <Link to="/games/valorant">Valorant</Link>
+              <Link to="/games/league-of-legends">League of Legends</Link>
+              <Link to="/games/teamfight-tactics">Overwatch</Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="social-cnt">
+          <div className="icons">
+            <a href="https://twitter.com/TAEesport">
+              <img src={twitter} alt="twitter-logo" className="twitter" />
+            </a>
+            <a href="https://www.twitch.tv/taeesports">
+              <img src={twitch} alt="twitch-logo" className="twitch" />
+            </a>
+            <a href="https://discord.com/invite/whUGyUygXm">
+              <img src={discord} alt="discord-logo" className="discord" />
+            </a>
+          </div>
+        </div>
+
+        <div className="rights-cnt">
+          <p>Lazy In Life &copy; 2023 All Rights Reserved</p>
+        </div>
+      </div>
+    );
+  }
 }
 
+export default Footer;
